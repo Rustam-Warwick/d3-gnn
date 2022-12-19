b3cd9c84a02280417f1129a986b9c90fe82eaf5c
rustambaku13 <q.rusta@code.edu.az>
2022-12-19 23:34:49 +0000

    HDRF and HyperGraphMinMax Fixed with main edits

--- src/test/java/benchmarks/MLTests.java
+++ src/test/java/benchmarks/MLTests.java
@@ -1,25 +1,19 @@
-package unit;
+package benchmarks;
 
 import ai.djl.ndarray.BaseNDManager;
 import ai.djl.ndarray.NDArray;
 import ai.djl.ndarray.NDManager;
 import ai.djl.ndarray.types.Shape;
-import org.bytedeco.javacpp.IntPointer;
 import org.junit.Test;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
-import org.tensorflow.*;
-import org.tensorflow.ndarray.FloatNdArray;
-import org.tensorflow.ndarray.NdArray;
-import org.tensorflow.ndarray.NdArrays;
+import org.tensorflow.Graph;
+import org.tensorflow.Result;
+import org.tensorflow.Session;
 import org.tensorflow.op.Ops;
-import org.tensorflow.op.Scope;
 import org.tensorflow.op.core.Placeholder;
-import org.tensorflow.op.math.Mul;
-import org.tensorflow.op.random.RandomUniform;
+import org.tensorflow.op.math.Add;
 import org.tensorflow.types.TFloat32;
-
-import java.util.List;
 
 public class MLTests {
 
@@ -27,17 +21,17 @@ public class MLTests {
     public void testTensorflow(){
         long ms = System.currentTimeMillis();
         Graph g = new Graph();
-        Ops tf = Ops.create(g);
-        Placeholder<TFloat32> input1 = tf.placeholder(TFloat32.class);
-        Placeholder<TFloat32> input2 = tf.placeholder(TFloat32.class);
-        Mul<TFloat32> res = tf.math.mul(input1, input2);
         Session session = new Session(g);
+        Ops tf = Ops.create(g);
+        TFloat32 arr1_float = TFloat32.tensorOf(org.tensorflow.ndarray.Shape.of(10,10));
+        TFloat32 arr2_float = TFloat32.tensorOf(org.tensorflow.ndarray.Shape.of(10,10));
+        Placeholder<TFloat32> arr1 = tf.placeholder(TFloat32.class);
+        Placeholder<TFloat32> arr2 = tf.placeholder(TFloat32.class);
+        Add<TFloat32> res =  tf.math.add(arr2, arr1);
         for (int i = 0; i < 100000; i++) {
-            TFloat32 arr2 = Tensor.of(TFloat32.class,org.tensorflow.ndarray.Shape.of(10,10));
-            TFloat32 arr1 = Tensor.of(TFloat32.class,org.tensorflow.ndarray.Shape.of(10,10));
-            List<Tensor> result = session.runner().feed(input1, arr1).feed(input2,arr2).run();
-            System.out.println(result.size());
-
+            Result a = session.runner().addTarget(res).fetch(res).feed(arr1, arr1_float).feed(arr2, arr2_float).run();
+//            arr2.scalars().forEach(item -> System.out.println(item.getObject()));
+//            res.asTensor().scalars().forEach(item -> System.out.println(item.getObject()));
         }
 
         System.out.println("Time taken for TF: " + (System.currentTimeMillis() - ms));
@@ -46,10 +40,10 @@ public class MLTests {
     @Test
     public void testNd4j(){
         long ms = System.currentTimeMillis();
+        INDArray arr1 = Nd4j.rand(10, 10);
+        INDArray arr2 = Nd4j.rand(10, 10);
         for (int i = 0; i < 100000; i++) {
-            INDArray arr1 = Nd4j.rand(10, 10);
-            INDArray arr2 = Nd4j.rand(10, 10);
-            arr1.mul(arr2);
+            arr1.add(arr2);
         }
         System.out.println("Time taken for ND4j: " + (System.currentTimeMillis() - ms));
     }
@@ -58,11 +52,13 @@ public class MLTests {
     public void testPyTorch(){
         long ms = System.currentTimeMillis();
         NDManager m = BaseNDManager.getManager();
+        m.delay();
+        NDArray arr1 = m.randomUniform(0, 1, new Shape(10,10));
+        NDArray arr2 = m.randomUniform(0, 1, new Shape(10,10));
         for (int i = 0; i < 100000; i++) {
-            NDArray arr1 = m.randomUniform(0, 1, new Shape(10,10));
-            NDArray arr2 = m.randomUniform(0, 1, new Shape(10,10));
-            arr1.mul(arr2);
+            arr1.add(arr2);
         }
+        m.resume();
         System.out.println("Time taken for PyTorch: " + (System.currentTimeMillis() - ms));
 
     }
