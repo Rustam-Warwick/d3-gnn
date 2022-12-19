18182ed3bcc651e835834b74a69bd6964270a56b
rustambaku13 <q.rustam@code.edu.az>
2022-12-19 17:01:59 +0000

    Working in production, want to move all NDManager delays to MailboxProcessor instead of StreamInputOperator

--- src/test/java/unit/MLTests.java
+++ src/test/java/unit/MLTests.java
@@ -4,22 +4,42 @@ import ai.djl.ndarray.BaseNDManager;
 import ai.djl.ndarray.NDArray;
 import ai.djl.ndarray.NDManager;
 import ai.djl.ndarray.types.Shape;
+import org.bytedeco.javacpp.IntPointer;
 import org.junit.Test;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
-import org.tensorflow.EagerSession;
-import org.tensorflow.Operand;
+import org.tensorflow.*;
+import org.tensorflow.ndarray.FloatNdArray;
+import org.tensorflow.ndarray.NdArray;
+import org.tensorflow.ndarray.NdArrays;
 import org.tensorflow.op.Ops;
-import org.tensorflow.types.TFloat64;
+import org.tensorflow.op.Scope;
+import org.tensorflow.op.core.Placeholder;
+import org.tensorflow.op.math.Mul;
+import org.tensorflow.op.random.RandomUniform;
+import org.tensorflow.types.TFloat32;
+
+import java.util.List;
+
 public class MLTests {
 
     @Test
     public void testTensorflow(){
-        Ops t = Ops.create(EagerSession.create());
         long ms = System.currentTimeMillis();
-        for (int i = 0; i < 100000 ; i++) {
-            Operand<TFloat64> res = t.math.mul(t.random.randomUniform(t.constant(new int[]{10,10}), TFloat64.class),t.random.randomUniform(t.constant(new int[]{10,10}),TFloat64.class));
+        Graph g = new Graph();
+        Ops tf = Ops.create(g);
+        Placeholder<TFloat32> input1 = tf.placeholder(TFloat32.class);
+        Placeholder<TFloat32> input2 = tf.placeholder(TFloat32.class);
+        Mul<TFloat32> res = tf.math.mul(input1, input2);
+        Session session = new Session(g);
+        for (int i = 0; i < 100000; i++) {
+            TFloat32 arr2 = Tensor.of(TFloat32.class,org.tensorflow.ndarray.Shape.of(10,10));
+            TFloat32 arr1 = Tensor.of(TFloat32.class,org.tensorflow.ndarray.Shape.of(10,10));
+            List<Tensor> result = session.runner().feed(input1, arr1).feed(input2,arr2).run();
+            System.out.println(result.size());
+
         }
+
         System.out.println("Time taken for TF: " + (System.currentTimeMillis() - ms));
     }
 
